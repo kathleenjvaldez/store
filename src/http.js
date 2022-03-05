c9fb256d47eaa9859dbe0a4af188b5ad5e43abd1
Kathleen Valdez <kathleen@Kathleens-MacBook-Air.local>
2022-03-04 20:12:57 -0800

    updated http to use env

--- src/http.js
+++ src/http.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+console.log(process.env.REACT_APP_API_URL);
+
+const baseURL = process.env.REACT_APP_API_URL
+  ? process.env.REACT_APP_API_URL
+  : "http://localhost:3001";
+
 const http = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL,
 });
 
 export default http;
